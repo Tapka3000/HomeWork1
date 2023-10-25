18f61482a74017f83f7e0b85a3a5397b00a02cff
Tapka3000 <circle.table@yandex.ru>
2023-10-25 18:33:34 +0300

    commit

---
+++ src/main/java/Square_Circle.java
@@ -0,0 +1,8 @@
+public class Square_Circle {
+    public static void main(String[] args) {
+        int r = 3;
+
+        double a = (double) (2 * Math.PI * r);
+        System.out.println("Площадь круг = " + a);
+    }
+}
---
+++ src/main/java/Square_Kvadrat.java
@@ -0,0 +1,9 @@
+public class Square_Kvadrat {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int c = a * b;
+
+        System.out.println("Площадь квадрата со сторонам " + a + " и " + b + " = " + c);
+    }
+}
